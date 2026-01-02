03cdc09ad9a7d1e9afbe7a13f2df13f5feb80d88
Clawwo <genzz760@gmail.com>
2026-01-02 12:15:05 +0700

    feat(api): menambahkan endpoint show notebook

--- server/internal/dto/notebook_dto.go
+++ server/internal/dto/notebook_dto.go
@@ -1,7 +1,12 @@
 package dto
 
-import "github.com/google/uuid"
+import (
+	"time"
 
+	"github.com/google/uuid"
+)
+
+// Request dan Response DTO untuk Notebook
 type CreateNotebookRequest struct {
 	Name string `json:"name" validate:"required"`
 	ParentId *uuid.UUID `json:"parent_id"`
@@ -9,4 +14,13 @@ type CreateNotebookRequest struct {
 
 type CreateNotebookResponse struct {
 	Id uuid.UUID `json:"id"`
+}
+
+// DTO untuk menampilkan detail notebook
+type ShowNotebookResponse struct {
+	Id uuid.UUID `json:"id"`
+	Name string `json:"name"`
+	ParentId *uuid.UUID `json:"parent_id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
